263838f05fc32d284d8ac09c7a6522b569421b95
thabangisaac1 <CSE17-008@thuto.bac.ac.bw>
2020-11-18 23:36:30 +0200

    Fragments

---
+++ app/src/main/java/com/example/do_i_need_it/MainActivity2.java
@@ -0,0 +1,46 @@
+package com.example.do_i_need_it;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bttm_nav);
+        bottomNavigationView.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
+    }
+
+   BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener = new
+           BottomNavigationView.OnNavigationItemSelectedListener() {
+               @Override
+               public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                   Fragment selectedFragment = null;
+
+                   switch (item.getItemId()){
+
+                       case R.id.nav_home:
+                           selectedFragment = new HomeFragment();
+                       break;
+                       case R.id.nav_profile:
+                           selectedFragment = new ProfileFragment();
+                           break;
+                   }
+                   getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+
+                   return  true;
+
+
+               }
+           };
+}
--- app/src/main/java/com/example/do_i_need_it/UserLogin.java
+++ app/src/main/java/com/example/do_i_need_it/UserLogin.java
@@ -5,15 +5,25 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class UserLogin extends AppCompatActivity {
     TextView txt;
+    Button loginbtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_login);
         txt = (TextView) findViewById(R.id.toregisterlink);
+        loginbtn = findViewById(R.id.loginbtn);
+
+        loginbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               openDashboard();
+            }
+        });
         txt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -23,6 +33,12 @@ public class UserLogin extends AppCompatActivity {
 
 
     }
+
+    private void openDashboard() {
+        Intent intent = new Intent(this, MainActivity2.class);
+        startActivity(intent);
+    }
+
     public void goToRegisterActivity() {
         Intent intent = new Intent(this, UserRegisteration.class);
         startActivity(intent);
